0468c7e40f3613e31434d764a537579d651f9039
coobila <51754595+coobila@users.noreply.github.com>
2022-06-09 14:16:18 +0800

    第一次上傳

---
+++ FCMdemo/FCM/FCMNotification.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using FirebaseAdmin.Messaging;
+using System.Threading.Tasks;
+
+/// <summary>
+/// FCMNotification 的摘要描述
+/// </summary>
+public class FCMNotification
+{
+
+    public FCMNotification()
+    {
+        //初始化SDK
+        if (FirebaseApp.DefaultInstance == null)
+        {
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile("add your json path")
+            });
+        }
+
+    }
+
+    /// <summary>
+    /// 向多台設備發送消息
+    /// </summary>
+    /// <param name="registrationTokens">設備Tokens</param>
+    /// <param name="title">主旨</param>
+    /// <param name="content">內容</param>
+    /// <returns></returns>
+    public async Task<string> PushMulticastNotificationAsync(List<String> registrationTokens, string title, string content)
+    {
+        //registrationTokens = "YOUR_REGISTRATION_TOKEN";
+
+        var message = new MulticastMessage()
+        {
+            Tokens = registrationTokens,
+            Notification = new Notification
+            {
+                Title = title,
+                Body = content
+            }
+        };
+
+        var response = await FirebaseMessaging.DefaultInstance.SendMulticastAsync(message);
+
+
+        return response.ToString();
+    }
+
+    /// <summary>
+    /// 向特定設備發送消息
+    /// </summary>
+    /// <param name="registrationToken"></param>
+    /// <param name="title"></param>
+    /// <param name="content"></param>
+    /// <returns></returns>
+    public async Task<string> PushNotificationAsync(string registrationToken, string title, string content)
+    {
+        //registrationToken = "YOUR_REGISTRATION_TOKEN";
+
+        var message = new Message()
+        {
+            Token = registrationToken,
+            Notification = new Notification
+            {
+                Title = title,
+                Body = content
+            }
+        };
+
+        var response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+
+
+        return response.ToString();
+    }
+
+}
---
+++ FCMdemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FCMdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FCM Server Start!");
+
+            //---S--- 向多台設備發送消息
+            FCMNotification fcmNotificationMgr = new FCMNotification();
+
+            // These registration tokens come from the client FCM SDKs.
+            var registrationTokens = new List<string>()
+            {
+                "YOUR_REGISTRATION_TOKEN_1",
+                // …
+                "YOUR_REGISTRATION_TOKEN_N",
+            };
+
+            string response = fcmNotificationMgr.PushMulticastNotificationAsync(registrationTokens, "test", "demo").Result;
+            Console.WriteLine("Successfully sent message: " + response);
+            //---E--- 向多台設備發送消息
+
+            ////---S--- 向特定設備發送消息
+            //FCMNotification fcmNotificationMgr = new FCMNotification();
+            //string response = fcmNotificationMgr.PushNotificationAsync("YOUR_REGISTRATION_TOKEN", "test", "demo").Result;
+            //Console.WriteLine("Successfully sent message: " + response);
+            ////---E--- 向特定設備發送消息
+        }
+
+    }
+}
